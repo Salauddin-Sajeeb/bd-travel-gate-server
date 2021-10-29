474c168b6739baef41b01b3ea03a4ce0ae46f14c
Salauddin Sajeebe <salauddin15-749@diu.edu.bd>
2021-10-30 02:17:55 +0600

    added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
